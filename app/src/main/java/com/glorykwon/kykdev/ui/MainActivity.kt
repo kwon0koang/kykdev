6eb8f05e0bc1db4c39f0589dd13dd0ae9f932ee4
kwon0koang <kwon0koang@gmail.com>
2022-06-14 00:42:07 +0900

    [kyk] android 12 splash 대응. splashscreen api 적용

--- app/src/main/java/com/glorykwon/kykdev/ui/MainActivity.kt
+++ app/src/main/java/com/glorykwon/kykdev/ui/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.glorykwon.kykdev.ui
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import com.glorykwon.kykdev.R
 import com.glorykwon.kykdev.databinding.MainActivityBinding
@@ -11,6 +16,8 @@ class MainActivity : BaseActivity() {
     private var mNavHostFragment: NavHostFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        showSplashScreen()      //android 12 부터 생긴 splash api
+
         super.onCreate(savedInstanceState)
         setContentView(mBinding.root)
 
@@ -23,7 +30,32 @@ class MainActivity : BaseActivity() {
         }
     }
 
+    private fun showSplashScreen() {
+        // Handle the splash screen transition.
+        installSplashScreen()
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // custom exit on splashScreen
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                // custom animation.
+                ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.height.toFloat()
+                ).apply {
+                    duration = 500
+                    // Call SplashScreenView.remove at the end of your custom animation.
+                    doOnEnd {
+                        splashScreenView.remove()
+                    }
+                }.run {
+                    // Run your animation.
+                    start()
+                }
+            }
+        }
+    }
 
 
 }
