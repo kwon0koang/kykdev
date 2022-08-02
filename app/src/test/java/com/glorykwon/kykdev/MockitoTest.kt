2cd3c4cac1f09dd6b071d4496d48aafd8cac9664
kwon0koang <kwon0koang@gmail.com>
2022-08-02 10:34:44 +0900

    [kyk] mockito test error 주석

--- app/src/test/java/com/glorykwon/kykdev/MockitoTest.kt
+++ app/src/test/java/com/glorykwon/kykdev/MockitoTest.kt
@@ -4,7 +4,7 @@ import com.glorykwon.kykdev.common.dto.TestDto
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito
+import org.mockito.Mockito.*
 
 
 /**
@@ -20,33 +20,35 @@ class MockitoTest {
 
     @Test
     fun `mock test`(){
-        val testDto = Mockito.mock(TestDto::class.java)
+        val testDto = mock(TestDto::class.java)
         assertEquals(true, testDto != null)
 
-        Mockito.`when`(testDto.value01).thenReturn("it is value 01")
-        Mockito.`when`(testDto.value02).thenReturn("it is value 02")
-        Mockito.`when`(testDto.value03).thenReturn("it is value 03")
+        `when`(testDto.value01).thenReturn("it is value 01")
+        `when`(testDto.value02).thenReturn("it is value 02")
+        `when`(testDto.value03).thenReturn("it is value 03")
 
         assertEquals("it is value 01", testDto.value01)
         assertEquals("it is value 02", testDto.value02)
         assertEquals("it is value 03", testDto.value03)
 
         //error
-        assertEquals("it is value 04", testDto.value03)
+//        assertEquals("it is value 04", testDto.value03)
     }
 
     @Test
     fun `mock test 02`(){
-        val testDto = Mockito.mock(TestDto::class.java)
+        val testDto = mock(TestDto::class.java)
         assertEquals(true, testDto != null)
 
-        Mockito.`when`(testDto.value01).thenReturn("it is value 01")
-        Mockito.`when`(testDto.value02).thenReturn("it is value 02")
-        Mockito.`when`(testDto.value03).thenThrow(RuntimeException("it is exception"))
+        `when`(testDto.value01).thenReturn("it is value 01")
+        `when`(testDto.value02).thenReturn("it is value 02")
+        `when`(testDto.value03).thenThrow(RuntimeException("it is exception"))
 
         assertEquals("it is value 01", testDto.value01)
         assertEquals("it is value 02", testDto.value02)
-        assertEquals("it is value 03", testDto.value03)
+
+        //error
+//        assertEquals("it is value 03", testDto.value03)
     }
 
 }
