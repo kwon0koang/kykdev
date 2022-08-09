32095d165f7aff13a0e060d6bda2f3a41ccb0970
kwon0koang <kwon0koang@gmail.com>
2022-08-09 23:56:37 +0900

    [kyk] interpreter pattern 주석 수정

--- app/src/main/java/com/glorykwon/kykdev/ui/designpatterntest/interpreter/DPInterpreter.kt
+++ app/src/main/java/com/glorykwon/kykdev/ui/designpatterntest/interpreter/DPInterpreter.kt
@@ -2,6 +2,10 @@ package com.glorykwon.kykdev.ui.designpatterntest.interpreter
 
 /**
  * 인터프리터 패턴
+ * 문법 규칙을 클래스화 한 구조로써, 일련의 규칙으로 정의된 언어를 해석하는 패턴
+ * 문법 규칙이 많아지면 복잡해지고 무거워지기 때문에 그럴 땐 차라리 파서/컴파일러 생성기를 쓰는 게 좋음
+ * 언어 분석기라고 생각하면 되며, 스크립트나 컴파일러 문법 등이 있을 수 있음
+ * 예로 SQL 구문이나 shell 커멘드 해석기, 통신 프로토콜 등이 있음
  */
 interface Expression {
     fun interpret(value: String): Boolean
