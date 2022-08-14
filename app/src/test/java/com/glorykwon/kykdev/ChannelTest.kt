2b667da559ef1352fa70b61c8ee354feab69f838
kwon0koang <kwon0koang@gmail.com>
2022-08-14 11:51:19 +0900

    [kyk] 코루틴 채널 팬인 팬아웃

--- app/src/test/java/com/glorykwon/kykdev/ChannelTest.kt
+++ app/src/test/java/com/glorykwon/kykdev/ChannelTest.kt
@@ -1,10 +1,7 @@
 package com.glorykwon.kykdev
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.channels.*
 import org.junit.Before
 import org.junit.Test
 
@@ -17,17 +14,38 @@ class ChannelTest {
     /**
      * 1씩 증가
      */
-    fun CoroutineScope.produceNumbers() = produce<Int> {
+    fun CoroutineScope.produceNumbers(interval: Long = 0) = produce<Int> {
         var x = 1
         while (true) {
             send(x++)
+            delay(interval)
+        }
+    }
+
+    /**
+     * from부터 2씩 증가
+     */
+//    suspend fun produceNumbers(channel: SendChannel<Int>, from: Int, interval: Long) {
+//        var x = from
+//        while (true) {
+//            channel.send(x)
+//            x += 2
+//            delay(interval)
+//        }
+//    }
+    fun CoroutineScope.produceNumbers(channel: SendChannel<Int>, from: Int, interval: Long) = produce<Int> {
+        var x = from
+        while (true) {
+            channel.send(x)
+            x += 2
+            delay(interval)
         }
     }
 
     /**
      * 홀수 필터링
      */
-    fun CoroutineScope.filterOdd(numbers: ReceiveChannel<Int>): ReceiveChannel<String> = produce {
+    fun CoroutineScope.produceFilterOdd(numbers: ReceiveChannel<Int>): ReceiveChannel<String> = produce {
         for (i in numbers) {
             if (i % 2 == 1) {
                 send("${i}!")
@@ -38,12 +56,63 @@ class ChannelTest {
     @Test
     fun `channel test`(): Unit = runBlocking {
         val numbers = produceNumbers()
-        val oddNumbers = filterOdd(numbers)
+
+        repeat(10) {
+            println(numbers.receive())
+        }
+        println("완료")
+        coroutineContext.cancelChildren()
+    }
+
+    @Test
+    fun `channel test (홀수 필터링)`(): Unit = runBlocking {
+        val numbers = produceNumbers()
+        val oddNumbers = produceFilterOdd(numbers)
 
         repeat(10) {
             println(oddNumbers.receive())
         }
         println("완료")
+        coroutineContext.cancelChildren()
+    }
+
+    /**
+     * fan-out : 여러 코루틴이 동시에 채널을 구독
+     */
+    @Test
+    fun `fan-out test`(): Unit = runBlocking {
+        val producer = produceNumbers(interval = 100)
+        repeat (5) {
+            launch {
+                producer.consumeEach {
+                    println("${it}가 ${it}을 받았습니다.")
+                }
+            }
+        }
+        delay(1000L)
+        producer.cancel()
+    }
+
+    /**
+     * fan-in : fan-out과 반대로 생산자가 많은 것
+     */
+    @Test
+    fun `fan-in test`(): Unit = runBlocking {
+        val channel = Channel<Int>()
+//        launch {
+//            produceNumbers(channel, 1, 100L)
+//        }
+//        launch {
+//            produceNumbers(channel, 2, 150L)
+//        }
+        produceNumbers(channel, 1, 100L)
+        produceNumbers(channel, 2, 150L)
+        launch {
+            channel.consumeEach {
+                println("${it}가 ${it}을 받았습니다.")
+            }
+        }
+        delay(1000L)
         coroutineContext.cancelChildren()
     }
 
