f6da6598be9630ff9a4d89c052163ffe39543248
kwon0koang <kwon0koang@gmail.com>
2022-08-29 17:36:36 +0900

    [kyk] espresso test 임시 커밋

---
+++ app/src/androidTest/java/com/glorykwon/kykdev/ui/MainActivityTest.kt
@@ -0,0 +1,16 @@
+package com.glorykwon.kykdev.ui
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Test
+    fun uiTest(): Unit = runBlocking {
+        // todo
+    }
+
+}
